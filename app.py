4373e99f597c71459536ef024c72c239d0ae7c2c
Where456 <119400636+Where456@users.noreply.github.com>
2023-04-03 16:09:05 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,17 +1,39 @@
 import os
-
-from flask import Flask
+from flask_restx import Resource, Namespace, Api
+from flask import Flask, request, jsonify
+from utils import filters
 
 app = Flask(__name__)
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 DATA_DIR = os.path.join(BASE_DIR, "data")
 
+data_ns = Namespace('perform_query')
+api = Api(app)
+api.add_namespace(data_ns)
 
-@app.route("/perform_query")
-def perform_query():
-    # получить параметры query и file_name из request.args, при ошибке вернуть ошибку 400
-    # проверить, что файла file_name существует в папке DATA_DIR, при ошибке вернуть ошибку 400
-    # с помощью функционального программирования (функций filter, map), итераторов/генераторов сконструировать запрос
-    # вернуть пользователю сформированный результат
-    return app.response_class('', content_type="text/plain")
+
+@data_ns.route("/")
+class PerformQuery(Resource):
+    def post(self):
+        response = request.json
+        filename = response["file_name"]
+        cmd1 = response["cmd1"]
+        value1 = response["value1"]
+        cmd2 = response["cmd2"]
+        value2 = response["value2"]
+        print(os.path.join(DATA_DIR, filename))
+        print()
+        with open(os.path.join(DATA_DIR, filename), 'r', encoding='utf-8') as file:
+            data = file.readlines()
+
+        res = filters(file=data, cmd=cmd1, value=value1)
+        print(res)
+        print()
+        res = filters(file=res, cmd=cmd2, value=value2)
+        print(res)
+        return jsonify(res)
+
+
+if __name__ == "__main__":
+    app.run()
